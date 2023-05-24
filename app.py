079a4a6aaea95b3c0683602a232ac164fb1eb9f2
José Anaya <60910800+JoseAnaya28@users.noreply.github.com>
2023-05-24 11:02:38 -0600

    Added range selection and avg/best price

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import streamlit as st
 import requests
 import pandas as pd
-from datetime import datetime
+from datetime import datetime, time
 import time
+
 
 def get_crypto_prices(date):
     ids = ['bitcoin', 'ethereum', 'dai', 'status', 'unicorn-token', 'superrare', 'the-graph', 'omisego', 'tether', 'radicle', 'staked-ether', 'weth', 'panvala-pan', 'sai', 'binance-usd', 'nym', 'rocket-pool-eth', 'usd-coin', 'wrapped-steth']
@@ -28,17 +29,62 @@ def get_crypto_prices(date):
         dataframes.append(pd.DataFrame({'ID': [id], 'Price in USD': [price_usd], 'Price in CHF': [price_chf]}))
 
         # Wait 3 seconds between requests
-        time.sleep(3)
+        time.sleep(5)
     
+    df = pd.concat(dataframes)
+    df.set_index('ID', inplace=True)
+    return df
+
+def unix_timestamp(date):
+    dt = datetime.combine(date, datetime.min.time())  # convert date to datetime with 00:00:00 time
+    return int(dt.timestamp())
+
+def get_crypto_prices_range(start_date, end_date):
+    ids = ['bitcoin', 'ethereum', 'dai', 'status', 'unicorn-token', 'superrare', 'the-graph', 'omisego', 'tether', 'radicle', 'staked-ether', 'weth', 'panvala-pan', 'sai', 'binance-usd', 'nym', 'rocket-pool-eth', 'usd-coin', 'wrapped-steth']
+    dataframes = []
+    
+    for id in ids:
+        url = f"https://api.coingecko.com/api/v3/coins/{id}/market_chart/range"
+        parameters = {
+            'vs_currency': 'usd',
+            'from': unix_timestamp(start_date),
+            'to': unix_timestamp(end_date)
+        }
+
+        response = requests.get(url, params=parameters)
+        data = response.json()
+
+        if 'prices' in data:
+            prices = [price[1] for price in data['prices']]  # get the price part of each [time, price] pair
+            avg_price = sum(prices) / len(prices)
+            best_price = max(prices)
+        else:
+            avg_price = best_price = None
+
+        dataframes.append(pd.DataFrame({'ID': [id], 'Average Price': [avg_price], 'Best Price': [best_price]}))
+
+        # Wait 3 seconds between requests
+        time.sleep(5)
+
     df = pd.concat(dataframes)
     df.set_index('ID', inplace=True)
     return df
 
 def main():
     st.title('Crypto Currency Rates')
+
     date = st.date_input('Select a date', datetime.now())
-    df = get_crypto_prices(date)
-    st.dataframe(df)
+    if st.button('Fetch Data for Selected Date'):
+        df = get_crypto_prices(date)
+        st.dataframe(df)
+
+    start_date, end_date = st.date_input('Select a date range', [datetime.now(), datetime.now()])
+    if st.button('Fetch Data for Date Range'):
+        st.subheader('Price Range Data')
+        df_range = get_crypto_prices_range(start_date, end_date)
+        st.dataframe(df_range)
 
 if __name__ == "__main__":
     main()
+
+
